a836c7a6a7dd69c7c932a9f30781bc45f66c1666
Gaurav Rajendra Girase <78299809+GauravGirase@users.noreply.github.com>
2026-03-19 11:20:23 +0530

    Update decorators.py

--- python/decorators.py
+++ python/decorators.py
@@ -1,1 +1,19 @@
+# Basic implemetation 
+from functools import wraps
+
+
+def decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print("Inside decorator")
+        result = func(*args, **kwargs)
+        return result*2
+    return wrapper
+
+
+@decorator
+def add(a,b):
+    return a+b
+
+print(add(5,5))
 
